d7f3bd15449aa3d8c4119eea465addf39ab13c3a
ivucicev <ivucicev@gmail.com>
2019-08-19 22:03:20 +0200

    prettier formatting & lint

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -3,27 +3,27 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class HomeService {
-  private API = environment.API;
-  constructor(private http: HttpClient) {}
+    private API = environment.API;
+    constructor(private http: HttpClient) {}
 
-  public getMovies(uid: string) {
-    const url = `${this.API}scrape/watchlist?userId=${uid}`;
-    const request = this.http.get(url);
-    return request;
-  }
+    public getMovies(uid: string) {
+        const url = `${this.API}scrape/watchlist?userId=${uid}`;
+        const request = this.http.get(url);
+        return request;
+    }
 
-  public getMovieOffers(title: string) {
-    const url = `${this.API}movie/offers?title=${title}`;
-    const request = this.http.get(url);
-    return request;
-  }
+    public getMovieOffers(title: string) {
+        const url = `${this.API}movie/offers?title=${title}`;
+        const request = this.http.get(url);
+        return request;
+    }
 
-  public getProviders() {
-    const url = `${this.API}providers`;
-    const request = this.http.get(url);
-    return request;
-  }
+    public getProviders() {
+        const url = `${this.API}providers`;
+        const request = this.http.get(url);
+        return request;
+    }
 }
