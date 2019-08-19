abc0135df60403b445929e63bd15dac9793bd4d9
ivucicev <ivucicev@gmail.com>
2019-08-19 22:32:36 +0200

    refactor storage from localstorage to ion-storage

--- src/app/cache.service.ts
+++ src/app/cache.service.ts
@@ -1,33 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CacheService {
-    public set<T>(key: string, value: T): void {
+    private readonly CACHE_EXPIRY = 1000 * 60 * 60 * 24; // 1 day
+
+    constructor(private storage: Storage) {}
+
+    public async set<T>(key: string, value: T): Promise<void> {
         if (
             (typeof value !== 'string' && typeof value !== 'number') ||
             typeof value !== 'boolean'
         ) {
-            window.localStorage.setItem(key, JSON.stringify(value));
+            return this.storage.set(key, JSON.stringify(value));
         } else {
-            window.localStorage.setItem(key, String(value));
+            return this.storage.set(key, String(value));
         }
     }
 
-    public get<T>(key: string): any {
-        try {
-            return JSON.parse(window.localStorage.getItem(key));
-        } catch (e) {
-            return window.localStorage.getItem(key);
-        }
+    public async get<T>(key: string): Promise<any> {
+        return this.storage.get(key);
     }
 
-    public remove(key: string): void {
-        window.localStorage.removeItem(key);
+    public async remove(key: string): Promise<void> {
+        return this.storage.remove(key);
     }
 
-    public clear(): void {
-        window.localStorage.clear();
+    public async clear(): Promise<void> {
+        return this.storage.clear();
     }
 }
